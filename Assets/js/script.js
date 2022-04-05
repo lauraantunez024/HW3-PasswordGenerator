8100ac48d9dbe52dfca4f453f63a61991f84f73f
Laura Antunez <lauraantunez024@gmail.com>
2022-04-04 21:20:11 -0400

    added ability to validate

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,15 +9,29 @@ function writePassword() {
   var characterTypes = [];
   let pwdLength = getLength();
   pwdLength = parseInt(pwdLength);
-  let isLower = lowerBool();
-  let isUpper = upperBool();
-  let isNum = numBool();
-  let isSpec = specBool();
 
   // check which character types are true
   // push the functions into an array
   // inside of loop, randomly select a function
   // inside of function, return the character
+  function selectCharacterTypes() {
+    let isLower = lowerBool();
+    let isUpper = upperBool();
+    let isNum = numBool();
+    let isSpec = specBool();
+    if (isLower) {
+      characterTypes.push(grabL) 
+    } 
+    if (isUpper) {
+      characterTypes.push(grabU) 
+    } 
+    if (isNum) {
+      characterTypes.push(grabN) 
+    } 
+    if (isSpec) {
+      characterTypes.push(grabS) 
+    }
+  }
 
   function grabU() {
 
@@ -39,17 +53,11 @@ function writePassword() {
 
       return special[Math.floor(Math.random() * special.length)];
   }
-  if (isLower === true) {
-    characterTypes.push(grabL) 
-  } 
-  if (isUpper === true) {
-    characterTypes.push(grabU) 
-  } 
-  if (isNum === true) {
-    characterTypes.push(grabN) 
-  } 
-  if (isSpec === true) {
-    characterTypes.push(grabS) 
+
+  selectCharacterTypes()
+  while (characterTypes.length === 0) {
+    alert("Input yes to at least one parameter")
+    selectCharacterTypes();
   }
 
   for (let i = 0; i < pwdLength; i++) {
@@ -79,52 +87,66 @@ var special = "~`!@#$%^&*()-_=+[]{'}/?>.<,;:|";
 // var length = document.querySelector("#length");
 
 
-
-  
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
 
 
 var getLength = function length() {
-  return prompt("How long should the password be?", "8-24");
+  let ans = prompt("How long should the password be?", "8-128");
+  while (parseInt(ans) < 8 || parseInt(ans) > 128){
+    alert("Must be between 8 and 128 characters.");
+    ans = prompt("How long should the password be?", "8-128");
+  } 
+  return ans;
 }
 
 
 var lowerBool = function lTrue (){
   let lowerInput = prompt("Include lowercase letters in password?", "Yes or No");
-  if (lowerInput.toLowerCase().trim() === "yes" ) {
-    return true;
-  } else {
-    return false;
-  }
+  let yesL = lowerInput.toLowerCase().trim() === "yes";
+  let inputBool = lowerInput.toLowerCase().trim() === "yes" || lowerInput.toLowerCase().trim() === "no";
+  while (!inputBool) {
+    lowerInput = prompt("Include lowercase letters in password?", "Yes or No");
+    inputBool = lowerInput.toLowerCase().trim() === "yes" || lowerInput.toLowerCase().trim() === "no";
+    yesL = lowerInput.toLowerCase().trim() === "yes";
+  } 
+  return yesL;
 }
 
 
 var upperBool = function uTrue (){
   let upperInput = prompt("Include uppercase letters in password?", "Yes or No");
-  if (upperInput.toLowerCase().trim() === "yes" ) {
-    return true;
-  } else {
-    return false;
-  }
+  let yesU = upperInput.toLowerCase().trim() === "yes";
+  let inputBool = upperInput.toLowerCase().trim() === "yes" || upperInput.toLowerCase().trim() === "no";
+  while (!inputBool) {
+    upperInput = prompt("Include uppercase letters in password?", "Yes or No");
+    inputBool = upperInput.toLowerCase().trim() === "yes" || upperInput.toLowerCase().trim() === "no";
+    yesU = upperInput.toLowerCase().trim() === "yes";
+  } 
+  return yesU;
 }
 
 var numBool = function nTrue (){
   let numInput = prompt("Include numbers in password?", "Yes or No");
-  if (numInput.toLowerCase().trim() === "yes" ) {
-    return true;
-  } else {
-    return false;
-  }
+  let yesN = numInput.toLowerCase().trim() === "yes";
+  let inputBool = numInput.toLowerCase().trim() === "yes" || numInput.toLowerCase().trim() === "no";
+  while (!inputBool) {
+    numInput = prompt("Include numbers in password?", "Yes or No");
+    inputBool = numInput.toLowerCase().trim() === "yes" || numInput.toLowerCase().trim() === "no";
+    yesN = numInput.toLowerCase().trim() === "yes";
+  } 
+  return yesN;
 }
 
 var specBool = function sTrue (){
   let specInput = prompt("Include special characters in password?", "Yes or No");
-  if (specInput.toLowerCase().trim() === "yes" ) {
-    return true;
-  } else {
-    return false;
-  }
+  let yesS = specInput.toLowerCase().trim() === "yes";
+  let inputBool = specInput.toLowerCase().trim() === "yes" || specInput.toLowerCase().trim() === "no";
+  while (!inputBool) {
+    specInput = prompt("Include special characters in password?", "Yes or No");
+    inputBool = specInput.toLowerCase().trim() === "yes" || specInput.toLowerCase().trim() === "no";
+    yesS = specInput.toLowerCase().trim() === "yes";
+  } 
+  return yesS;
 }
 
